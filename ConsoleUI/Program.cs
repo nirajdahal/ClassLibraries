c22762f9d4291d780f9e11ed7a2031d02ef7263f
nirajdahal09@gmail.com <nirajdahal09@gmail.com>
2020-07-29 18:03:20 +0545

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DemoLibrary.Models;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+           
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DemoLibrary/Calculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace DemoLibrary
+{
+    public static class Calculation
+    {
+
+        public static string AskInput(string message)
+        {
+            Console.WriteLine(message);
+            string output = Console.ReadLine();
+            return output;
+        }
+        public static double AddNum(double num1, double num2)
+        {
+            double output = num1 + num2;
+            return output;
+        }
+    }
+}
